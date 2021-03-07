223983741969651f573e31f05cc726e59b4e4f9b
mishathefirst <30623681+mishathefirst@users.noreply.github.com>
2021-03-07 13:34:47 +0300

    Sinus class implemented

--- src/main/java/test/Main.java
+++ src/main/java/test/Main.java
@@ -1,7 +1,10 @@
 package test;
 
+import test.basic.Sinus;
+
 public class Main {
     public static void main(String[] args) {
-
+        Sinus sinus = new Sinus();
+        System.out.println(sinus.getSinus(3.14));
     }
 }
--- src/main/java/test/basic/Sinus.java
+++ src/main/java/test/basic/Sinus.java
@@ -1,7 +1,39 @@
 package test.basic;
 
 public class Sinus {
+    double arg = 0, value = 0;
 
+    public Sinus() {}
+    public Sinus (double arg) {
+        this.arg = arg;
+        this.value = getSinus(arg);
+    }
 
+    public double getValue() {
+        return value;
+    }
+
+    public double getSinus(double arg) {
+        this.arg = arg;
+        int k = 0;
+        double previousValue = 0;
+        //while (Math.abs(value - previousValue) > Math.pow(10, -5)) {
+        while (k < 5) {
+            previousValue = value;
+            value = value + ((Math.pow(-1, k) * Math.pow(arg, (2 * k + 1)) / getFactorial(2 * k + 1)));
+            k++;
+        }
+        return value;
+    }
+
+    private int getFactorial(int arg) {
+        if (arg == 0 || arg == 1) {
+            return 1;
+        } else if (arg == 2) {
+            return 2;
+        } else {
+            return getFactorial(arg - 1) * arg;
+        }
+    }
 
 }
