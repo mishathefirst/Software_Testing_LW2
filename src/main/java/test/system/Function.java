3accfb3062532569cdcd127584746964d54a4dfb
mishathefirst <30623681+mishathefirst@users.noreply.github.com>
2021-03-08 19:21:05 +0300

    Function class finished

--- src/main/java/test/system/Function.java
+++ src/main/java/test/system/Function.java
@@ -1,8 +1,8 @@
 package test.system;
 
+import test.basic.NaturalLogarithm;
 import test.basic.Sinus;
-import test.derivative_functions.Log2;
-import test.derivative_functions.Tangent;
+import test.derivative_functions.*;
 
 public class Function {
 
@@ -11,15 +11,10 @@ public class Function {
         if (x <= 0) {
             return ((getSinus(x) / getTan(x)) / getTan(x));
         } else {
-/*
             return (((((log_3(x) + log_2(x)) / ln(x)) * (log_2(x) * ln(x))) +
-                    (log_10(x) * ((log_10(x) * log_3(x)) ^ 3))) -
-                    (((log_5(x) / log_3(x)) * ((log_2(x) / log_10(x)) ^ 3)) ^ 2));
-
- */
-
+                    (log_10(x) * (Math.pow((log_10(x) * log_3(x)), 3))) -
+                    (Math.pow((log_5(x) / log_3(x)) * (Math.pow((log_2(x) / log_10(x)), 3)), 2))));
         }
-        return 0;
     }
 
     private double getSinus(double x) {
@@ -32,6 +27,22 @@ public class Function {
 
     private double log_2(double x) {
         return new Log2().getLog2(x);
+    }
+
+    private double log_3(double x) {
+        return new Log3().getLog3(x);
+    }
+
+    private double log_5 (double x) {
+        return new Log5().getLog5(x);
+    }
+
+    private double log_10 (double x) {
+        return new Log10().getLog10(x);
+    }
+
+    private double ln (double x) {
+        return new NaturalLogarithm().getNLogarithm(x);
     }
 
 }
