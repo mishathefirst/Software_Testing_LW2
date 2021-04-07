52ebf34fa08b3e31605ab0fe6d42c8546b7029e0
mishathefirst <30623681+mishathefirst@users.noreply.github.com>
2021-04-08 02:30:16 +0300

    First mock added

--- src/test/java/LogarithmTesting.java
+++ src/test/java/LogarithmTesting.java
@@ -1,4 +1,17 @@
+import org.junit.Test;
+import test.basic.NaturalLogarithm;
+
+import static junit.framework.TestCase.assertEquals;
+
+
 public class LogarithmTesting {
+
+    NaturalLogarithm naturalLogarithm = new NaturalLogarithm();
+
+    @Test
+    public void check1NL() {
+        assertEquals(1,1);
+    }
 
 
 
